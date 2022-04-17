6ca1ea13e231f9b15b98e5801f680f1a88c6d0cb
edy20018 <77228962+edy20018@users.noreply.github.com>
2022-04-17 21:50:21 +0300

    Initial commit

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WepApiServer.Models;
+
+namespace WepApiServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FileUploadController : ControllerBase
+    {
+        public static IWebHostEnvironment? _webHostEnvironment;
+
+        public FileUploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost,Authorize(Roles = "Admin")]
+        public string Post([FromForm]FileUpload objectFile)
+        {
+            try {
+                if (objectFile.files.Length > 0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\versions\\";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(path + objectFile.files.FileName))
+                    {
+                        objectFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "Uploaded.";
+                    }
+                }
+                else
+                {
+                    return "Could not upload.";
+                }
+
+            }
+            catch (Exception ex){
+                return ex.Message;
+            }
+
+        }
+    }
+}
---
+++ obj/Release/net6.0/PubTmp/Out/wwwroot/versions/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include "Header.h"
+using namespace std;
+
+
+bool IssEmpty(const queue& Q)
+{
+	return Q.head == Q.tail;
+}
+bool IssFull(const queue& Q)
+{
+	return Q.tail == dimmax - 1;
+}
+
+void InitQ(queue& Q)
+{
+	Q.tail = 0;
+	Q.head = 0;
+}
+
+void Put(queue& Q, atom a)
+{
+	Q.vect[Q.tail] = a;
+	Q.tail=NextPos(Q.tail);
+	if (IssFull(Q))
+		InitQ(Q);
+}
+
+atom Get(queue& Q)
+{
+	atom a = Q.vect[Q.head];
+	Q.head = NextPos(Q.head);
+	if (IssEmpty(Q)==true)
+		InitQ(Q);
+	return a;
+}
+
+atom Front(const queue& Q)
+{
+	return Q.vect[Q.head];
+}
+
+int NextPos(int index)
+{
+	if (index < dimmax - 1)//altă soluție??
+		return index + 1;
+	else return 0;
+}
